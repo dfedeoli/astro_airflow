041265ba030cf7be9ba83ce5030b926b01383b74
dfedeoli <danilo_f_oliveira@hotmail.com>
2023-06-04 10:22:23 -0300

    updates on DAG file

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
@@ -10,7 +11,11 @@ default_args = {
 }
 
 def _downloading_data(**kwargs):
-    print(kwargs)
+    with open('/tmp/my_file.txt', 'w') as f:
+        f.write('my_data')
+
+# def _downloading_data(**kwargs):
+#     print(kwargs)
 
 # def _downloading_data(my_param, ds):
 #     print(my_param)
@@ -18,21 +23,15 @@ def _downloading_data(**kwargs):
 with DAG(dag_id='simple_dag', schedule_interval="@daily", 
         start_date=days_ago(3), catchup=False) as dag:
 
-    # task_1 = DummyOperator(
-    #     task_id='task_1',
-    # )
-
-    # task_2 = DummyOperator(
-    #     task_id='task_2',
-    #     retry=3,
-    # )
-
-    # task_3 = DummyOperator(
-    #     task_id='task_3',
-    # )
-
     downloading_data = PythonOperator(
         task_id='downloading_data',
         python_callable=_downloading_data,
         # op_kwargs={'my_param': 42}
+    )
+
+    waiting_for_data = FileSensor(
+        task_id='waiting_for_data',
+        fs_conn_id='fs_default',
+        filepath='my_file.txt',
+        # poke_interval=15
     )
